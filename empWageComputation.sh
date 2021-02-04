ff55b33e7abaf6fa5bb567f76f4d1e4bd674d302
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-02-04 18:45:36 +0530

    Refactor - attendance functionality added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+if [ $isPresent -eq $randomCheck ];
+then
+     echo "Employee is Present"
+else
+     echo "Employee is Abscent"
+fi
